06bb3c0dfc5775e9c721ff8a7f0c5b2acc8c7578
sylla <s67986601@gmail.com>
2021-02-08 11:51:51 +0000

    fin du carousel big mais petit probleme sur le smal

--- modele/class/router.php
+++ modele/class/router.php
@@ -1,9 +1,11 @@
 <?php
+
 namespace aps;
 
 use AltoRouter;
 
-class router{
+class router
+{
     private $url_name;
     private $callback_url;
     private $route;
@@ -13,48 +15,49 @@ class router{
         $this->url_name = $url_name;
     }
 
-    public function execute (){
+    public function execute()
+    {
         $this->init_router();
         $this->element_router();
         $this->exec_element_router();
     }
+
     
-    private function init_router(){
+    private function init_router()
+    {
         $this->route = new AltoRouter();
     }
 
 
-    private function element_router(){
+    private function element_router()
+    {
         foreach ($this->url_name as $key => $value) {
-            $this->route->map('GET|POST',$value,function(){
+            $this->route->map('GET|POST', $value, function () {
                 $redirection = $_SERVER["PATH_INFO"] ?? "/../controller/acceuill";
-                  require_once   __DIR__."/../../controller".$redirection."/index.php";
+                require_once   __DIR__ . "/../../controller" . $redirection . "/index.php";
             });
         }
     }
 
 
 
-    private function exec_element_router(){
+    private function exec_element_router()
+    {
 
 
         foreach ($this->url_name as $key => $value) {
 
-            if ($key>=0){
+            if ($key >= 0) {
                 $this->callback_url = $this->route->match();
             }
-           
-            if ( isset($this->callback_url['target'])){
+
+            if (isset($this->callback_url['target'])) {
                 $this->callback_url['target']();
                 break;
-            }
-            
-            else{
+            } else {
                 require_once  "../controller/error/index.php";
                 break;
             }
         }
     }
-
-
 }
--- public/script/slide.js
+++ public/script/slide.js
@@ -1,49 +1,56 @@
-let header = document.querySelector('header');
+let header = document.querySelector("header");
 header.style.width = innerWidth + "px";
 window.onresize = () => {
     header.style.width = innerWidth + "px";
-}
+};
+
 //pour le carousel
-let carousel = document.querySelector('.carousel');
-let carousel_main = document.querySelector('.carousel_main');
-let carousel_main_first = document.querySelector('.carousel_main--first');
-let carousel_main_first__chlidAll = document.querySelectorAll('.child');
+let carousel = document.querySelector(".carousel");
+let carousel_main = document.querySelector(".carousel_main");
+let carousel_main_first = document.querySelector(".carousel_main--first");
+let carousel_main_first__chlidAll = document.querySelectorAll(".child");
+
+// pour venir en arriere
+let prev_carousel = document.querySelector(".btn1");
+// pour partir en avant
+let next_carousel = document.querySelector(".btn2");
+// les element qui vont nous permettre de partie sur un element precise
+let btn_element_carousel = document.querySelector('.element_carousel');
+
 
 //pour le menu pour les categories
-let slider = document.querySelector('.slider');
-let menu = document.querySelector('#menu');
+let slider = document.querySelector(".slider");
+let menu = document.querySelector("#menu");
 
 // la partie connection pour dire se connection inscription
-let sign = document.querySelector('.sign');
-let sign_up = document.querySelector('#sign');
+let sign = document.querySelector(".sign");
+let sign_up = document.querySelector("#sign");
 
 // la patir resize d bloc de text quand on click sur un button
-let main_bloc = document.querySelector('.resize-bloc');
+let main_bloc = document.querySelector(".resize-bloc");
 //  les icone du header pour le resize
-let head_main = document.querySelector('.head_main');
-let head_second = document.querySelector('.head_second');
-let icone = document.querySelector('.iconeS');
-let icone_back = document.querySelector('.first_back')
-
+let head_main = document.querySelector(".head_main");
+let head_second = document.querySelector(".head_second");
+let icone = document.querySelector(".iconeS");
+let icone_back = document.querySelector(".first_back");
 
 // la variabe qui va servir de count gobale de click for gerer les click
 let global_click = [];
 let click_slider = 0;
 
 // les variable qui font bouge le carousel
-let compt = carousel.scrollWidth / carousel_main_first__chlidAll.length;
+if (carousel != null) {
+    let compt = carousel.scrollWidth / carousel_main_first__chlidAll.length;
+}
 let taille = 0;
 
-menu.addEventListener('click', move);
-window.addEventListener('resize', resize(menu));
+menu.addEventListener("click", move);
+window.addEventListener("resize", resize(menu));
 
-sign_up.addEventListener('click', sig);
+sign_up.addEventListener("click", sig);
 
-icone.addEventListener('click', icon);
-icone_back.addEventListener('click', icon_back);
-
-
-
+icone.addEventListener("click", icon);
+icone_back.addEventListener("click", icon_back);
 
 window.onclick = () => {
     setTimeout(() => {
@@ -62,8 +69,7 @@ window.onclick = () => {
     // if (click_slider == true){
     //     console.log(slider.clientWidth);
     // }
-
-}
+};
 
 function move() {
     if (slider.attributes.class.textContent != "slider") {
@@ -72,27 +78,35 @@ function move() {
         menu.style.transform = "rotate(90deg)";
         slider.attributes.class.textContent = "slider";
         slider.style.opacity = 0;
-        carousel.style.overflow = "hidden";
+        if (carousel != null) {
+            carousel.style.overflow = "hidden";
+        }
         setTimeout(() => {
             slider.style.width = "45vh";
             slider.style.opacity = 1;
+            btn_element_carousel.style.left = carousel_main_first__chlidAll[0].clientWidth / 2.2 + 'px';
         }, 100);
         setTimeout(() => {
             if (innerWidth > 1024) {
-                main_bloc.style.width = (innerWidth - slider.clientWidth - 20) + "px";
-                carousel_main.style.width = (innerWidth - slider.clientWidth + 10) + "px";
-
+                if (main_bloc != null) {
+                    main_bloc.style.width = innerWidth - slider.clientWidth - 20 + "px";
+                }
+                if (carousel != null) {
+                    carousel_main.style.width =
+                        innerWidth - slider.clientWidth + 10 + "px";
+                }
             }
         }, 600);
-
-
     } else {
-        document.querySelector('.btn2').style.left = innerWidth + "px"
         global_click.pop();
         if (innerWidth < 1024) {
-            carousel.style.overflowX = "scroll";
+            if (carousel != null) {
+                carousel.style.overflowX = "scroll";
+            }
         }
-        main_bloc.style.width = 100 + "%";
+        if (main_bloc != null) {
+            main_bloc.style.width = 100 + "%";
+        }
         click_slider = false;
         menu.style.transform = "rotate(180deg)";
         slider.style.width = "0.2%";
@@ -100,69 +114,83 @@ function move() {
         setTimeout(() => {
             slider.attributes.class.textContent += " no-click";
             slider.style.opacity = 0;
+            btn_element_carousel.style.left = innerWidth / 2.5 + 'px';
+
         }, 400);
     }
 }
 
-
-
-
 function resize(menu) {
     setTimeout(() => {
-        //pour le click au grand ecran 
-        if (innerWidth >= 1024 && location.pathname == "/" && (global_click != null)) {
+        //pour le click au grand ecran
+        if (
+            innerWidth >= 1024 &&
+            location.pathname == "/" &&
+            global_click != null
+        ) {
             menu.click();
         }
     }, 1000);
 
     //pour le carousel des petit ecran
-    if (innerWidth < 1024) {
+    if (carousel != null) {
+        if (innerWidth < 1024) {
+            (function caeousel_move() {
+                setInterval(() => {
+                    carousel.scrollLeft = taille;
+                    taille += compt;
 
-        (function caeousel_move() {
-            setInterval(() => {
-                carousel.scrollLeft = taille;
-                taille += compt;
+                    if (taille >= carousel.scrollWidth) {
+                        taille = 0;
+                    }
+                }, 4000);
+            })();
+        }
+        // pour les tablettee
 
-                if (taille >= carousel.scrollWidth) {
-                    taille = 0;
-                }
-            }, 4000);
+        if (innerWidth >= 1024 && screen.orientation.type == "landscape-primary") {
+            carousel_main.style.width = innerWidth - 100 + "px";
+        }
 
-        })();
-    }
-    // pour les tablettee
+        if (innerWidth >= 1024) {
+            carousel.style.width =
+                carousel_main_first__chlidAll[0].lastElementChild.clientWidth + "px";
+            prev_carousel.style.top = innerHeight / 2.5 + "px";
+            next_carousel.style.top = innerHeight / 2.5 + "px";
+            btn_element_carousel.style.top = innerHeight / 1.5 + 'px';
 
-    if (innerWidth >= 1024 && screen.orientation.type == "landscape-primary") {
-        carousel_main.style.width = innerWidth - 100 + "px";
-    }
-
-    if(innerWidth >= 1024){
-        carousel.style.width = carousel_main_first__chlidAll[0].lastElementChild.clientWidth + "px";
-        document.querySelector('.btn').style.top = innerHeight / 2.5 + "px";
-        document.querySelector('.bt').style.top = innerHeight / 2.5 + "px";
+        }
     }
 }
-if(innerWidth >= 1024){
-    carousel.style.width = carousel_main_first__chlidAll[0].lastElementChild.clientWidth + "px";
-}
 
+// btn_element_carousel.style.left = carousel_main_first__chlidAll[0].clientWidth/2.2 + 'px';
+if (carousel != null) {
+    if (innerWidth >= 1024) {
+        carousel.style.width =
+            carousel_main_first__chlidAll[0].lastElementChild.clientWidth + "px";
+    }
+}
 
 function sig() {
     if (sign.attributes.class.textContent != "sign") {
-        global_click.push("sign")
+        global_click.push("sign");
 
         sign.attributes.class.textContent = "sign";
         sign.style.opacity = 1;
-        carousel.style.overflow = "hidden"
-
-
+        if (carousel != null) {
+            carousel.style.overflow = "hidden";
+        }
 
         setTimeout(() => {
             sign.style.width = "50vh";
         }, 100);
     } else {
         global_click.pop();
-        carousel.style.overflow = "scroll";
+        if (innerWidth < 1024) {
+            if (carousel != null) {
+                carousel.style.overflowX = "scroll";
+            }
+        }
         sign.style.width = "0%";
         setTimeout(() => {
             sign.attributes.class.textContent += " no-click";
@@ -170,7 +198,6 @@ function sig() {
         }, 400);
     }
 }
-
 
 function icon() {
     let click = true;
@@ -184,27 +211,89 @@ function icon_back() {
     head_main.attributes.class.textContent = "head_main";
     head_second.style.display = "none";
 }
-window.onresize = () => {
 
-    (function carousel_big() {
-        if(innerWidth > 1024){
-            setInterval(() => {
-                carousel_main.lastElementChild.style.transform = "translateX("+-300+"vh)";
-            }, 1000);
-        }
-    })()
+
+if (carousel != null) {
+    window.onresize = () => {
+        (function carousel_big() {
+            if (innerWidth > 1024) {
+                setInterval(() => {
+                    carousel_main.lastElementChild.style.transform =
+                        "translateX(" + -300 + "vh)";
+                }, 3000);
+            }
+        })();
+    };
 }
 
-if(innerWidth > 1024){
-    let avance = 0;
-    let time = carousel_main_first__chlidAll.length;
-    setInterval(() => {
-        time--;
-        carousel_main.lastElementChild.style.transform = "translateX("+avance+"vh)";
-        avance+=150;
-        if(!time){
-            avance = 0;
-            time = carousel_main_first__chlidAll.length;
-        }
-    }, 1000);
+
+let time = carousel_main_first__chlidAll.length;
+if (carousel != null) {
+    if (innerWidth > 1024) {
+        let avance = 0;
+        setInterval(() => {
+            time--;
+            carousel_main.lastElementChild.style.transform =
+                "translateX(" + avance + "vh)";
+            avance += 150;
+            if (!time) {
+                avance = 0;
+                time = carousel_main_first__chlidAll.length;
+            }
+        }, 5000);
+
+        next_carousel.onclick = () => {
+            if (time <= 1) {
+                next_carousel.style.display = "block";
+                next_carousel.style.display = "none";
+            } else {
+                prev_carousel.style.display = "block";
+                time--;
+                avance += 150;
+                carousel_main.lastElementChild.style.transform =
+                    "translateX(" + avance + "vh)";
+            }
+        };
+
+        prev_carousel.onclick = () => {
+            if (time >= carousel_main_first__chlidAll.length) {
+                next_carousel.style.display = "block";
+                prev_carousel.style.display = "none";
+            } else {
+                next_carousel.style.display = "block";
+                time++;
+                avance -= 150;
+                carousel_main.lastElementChild.style.transform =
+                    "translateX(" + avance + "vh)";
+            }
+        };
+    }
+}
+
+if (carousel != null) {
+    if (innerWidth > 1024) {
+        (function () {
+            for (let i = 0; i < carousel_main_first__chlidAll.length; i++) {
+                btn_element_carousel.innerHTML += "<p class=" + i + " id='ok'><p>";
+            }
+            for (let i = 0; i < carousel_main_first__chlidAll.length; i++) {
+                document.getElementsByClassName(i)[0].onclick = (e) => {
+                    for (let index = 0; index < carousel_main_first__chlidAll.length; index++) {
+                        document.getElementsByClassName(index)[0].style.background = "white";
+                    }
+                    document.getElementsByClassName(i)[0].style.background = "#0096c7";
+                    
+                    console.log(this);
+                    e.target.click();
+                    time = carousel_main_first__chlidAll.length - i;
+
+                    avance = i * 150;
+                    carousel_main.lastElementChild.style.transform =
+                        "translateX(" + avance + "vh)";
+                };
+            }
+            
+
+        })();
+    }
 }
